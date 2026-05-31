788a0e5458d2ea52e87a6e6a449cfb838dc32193
Jacob Nelson <jacobanelson10@gmail.com>
2026-05-31 00:09:32 -0700

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        front, back = 0, len(s) - 1
+
+        while front < back:
+
+            if not s[front].isalnum():
+                front += 1
+                continue
+            if not s[back].isalnum():
+                back -= 1
+                continue
+            
+            if s[front].lower() != s[back].lower():
+                return False
+            
+            front += 1
+            back -= 1
+
+        return True
+        
