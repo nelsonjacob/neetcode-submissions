b40e9634d1a59f95b2ebf776ba20f54379b50d16
Jacob Nelson <jacobanelson10@gmail.com>
2026-05-30 23:10:55 -0700

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,43 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+
+
+        '''
+        anagram: str that contains the exact same chars as another str, ordering can differ
+
+        s: str, t: str
+
+        s and t consist of lowercase English letters.
+        '''
+
+        if len(s) != len(t):
+            return False
+
+        seen_chars = [0] * 26
+
+        # 'a' - [1, 0, ...]
+
+
+        for s_char in s:
+            seen_chars[ord(s_char) - ord('a')] += 1
+
+
+        for t_char in t:
+
+            if seen_chars[ord(t_char) - ord('a')] == 0:
+                return False
+            
+            seen_chars[ord(t_char) - ord('a')] -= 1
+
+
+        return True
+        
+
+        
+
+
+
+
+        
+
