f4e121cf8d1d804ea7e018c57be8e51ec893bf11
Jacob Nelson <jacobanelson10@gmail.com>
2026-06-06 12:50:53 -0700

    Add: islands-and-treasure - submission-2

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-2.py
@@ -0,0 +1,70 @@
+from collections import deque
+
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+
+
+
+        '''
+        -1 - A water cell that can not be traversed.
+0 - A treasure chest.
+INF - A land cell that can be traversed. We use the integer 2^31 - 1 = 2147483647 to represent INF.
+
+        '''
+
+
+
+        if not grid:
+            return
+
+
+        
+        WATER = -1
+        TREASURE = 0
+        INF = 2147483647
+        neighbors = [(1,0),(-1,0),(0,1),(0,-1)]
+
+        rows, cols = len(grid), len(grid[0])
+
+
+        treasure_cells = []
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == TREASURE:
+                    treasure_cells.append((r, c))
+        
+
+        queue = deque(treasure_cells)
+
+        while queue:
+
+            cells = len(queue)
+            for i in range(cells):
+                row, col = queue.popleft()
+
+                for ri, ci in neighbors:
+                    if 0 <= row + ri < rows and 0 <= col + ci < cols and grid[row+ri][col+ci] == INF:
+                        grid[row+ri][col+ci] = grid[row][col] + 1
+                        
+                        queue.append((row + ri, col + ci))
+
+            
+        
+        return
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
