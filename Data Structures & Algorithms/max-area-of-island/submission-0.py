69433287e67e41d6b8e1d1990dc528c31e6de24a
Jacob Nelson <jacobanelson10@gmail.com>
2026-06-15 19:22:01 -0700

    Add: max-area-of-island - submission-0

---
+++ Data Structures & Algorithms/max-area-of-island/submission-0.py
@@ -0,0 +1,55 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+
+
+        if not grid:
+            return 0
+
+        rows, cols = len(grid), len(grid[0])
+        LAND = 1
+        WATER = 0
+
+
+        neighbors = [(1,0), (-1,0), (0,1), (0, -1)]
+        max_island_area = 0
+
+        def get_island_area(row, col):
+            
+
+
+
+            island_size = 1
+            grid[row][col] = WATER
+
+            for r1, c1 in neighbors:
+
+                newRow, newCol = row+r1, col+c1
+
+                if 0 <= newRow < rows and 0 <= newCol < cols and grid[newRow][newCol] == LAND:
+                    island_size = island_size + get_island_area(newRow, newCol)
+                    
+
+
+
+
+            return island_size
+
+
+
+
+
+
+
+
+
+
+
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == LAND:
+                    max_island_area = max(max_island_area, get_island_area(row, col))
+
+
+
+        return max_island_area
+        
